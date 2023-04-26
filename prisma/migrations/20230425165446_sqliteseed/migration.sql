1c52c4f003c91fb8540634d02ee361046f1b9ccc
Lars Larsen <lars@lzxindustries.net>
2023-04-25 23:49:10 -0700

    Switched to MongoDB

--- prisma/migrations/20230425165446_sqliteseed/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Patch" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "name" TEXT
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Patch_email_key" ON "Patch"("email");
