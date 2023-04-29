43b442bf51dca852fc869c18d88cf5e9e36bd950
Lars Larsen <lars@lzxindustries.net>
2023-04-29 11:46:20 -0700

    tsconfig added to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,32 @@
-import type {StorybookConfig} from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-webpack5";
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../app/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', {
-    name: '@storybook/addon-styling',
-    options: {
-      // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-      // For more details on this addon's options.
-      postCss: true
+  stories: ["../stories/**/*.mdx", "../app/components/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ['../public'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions", {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true
+      }
     }
-  }],
+  ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: "@storybook/react-webpack5",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
+  webpackFinal: async (config, { configType }) => {
+       config.resolve.plugins = [new TsconfigPathsPlugin()];
+       return config;
+  }
 };
+
+export default config;
