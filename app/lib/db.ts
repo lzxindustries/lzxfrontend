8209cd6de03f953617b17613769f70544e7ac36d
Lars Larsen <lars@lzxindustries.net>
2023-04-30 13:06:47 -0700

    Add static JSON file db

---
+++ app/lib/db.ts
@@ -0,0 +1,34 @@
+export const db = {
+    "modules": {
+        "angles": {
+            "name": "Angles",
+            "sku": "LZXANGLES",
+            "dimensions":
+            {
+                "horizontal-pitch": 12,
+                "mounting-depth": 42,
+            },
+            "power-consumption": 
+            {
+                "milliamps-positive-12-volts" : 120,
+                "milliamps-negative-12-volts" : 0
+            }
+        },
+        "fkg3": {
+            "name": "FKG3 Keyer",
+            "sku": "LZXFKG3",
+            "dimensions":
+            {
+                "horizontal-pitch": 12,
+                "mounting-depth": 42,
+            },
+            "power-consumption": 
+            {
+                "milliamps-positive-12-volts" : 120,
+                "milliamps-negative-12-volts" : 0
+            }
+        }
+    }
+};
+
+
