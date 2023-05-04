7d7cf926ee94658d7d2308ef6952ff648a75c578
Lars Larsen <lars@lzxindustries.net>
2023-05-04 10:08:34 -0700

    Add assets, add Markdown to HTML

--- app/routes/apitest.tsx
+++ app/routes/apitest.tsx
@@ -1,6 +1,11 @@
 import { LoaderArgs } from "@shopify/remix-oxygen";
 import { useLoaderData } from "@remix-run/react";
 import { getDataCollection } from "~/lib/db.common.server";
+import { Remarkable } from 'remarkable';
+import { useEffect, useState } from "react";
+//import raw from "raw.macro";
+//const markdown = raw("../../public/docs/beginner-patches.md");
+
 
 export async function loader({ context }: LoaderArgs) {
 
@@ -12,12 +17,26 @@ export async function loader({ context }: LoaderArgs) {
   };
 };
 
+export function getMarkdownToHTML(url: string) {
+  const [markdownString, setMarkdownString] = useState('');
+  useEffect(() => {
+    fetch('/docs/beginner-patches.md')
+      .then((response) => response.text())
+      .then((text) => {
+        setMarkdownString(text);
+      });
+  }, []);
+
+  const markdownRenderer = new Remarkable();
+  const response = markdownRenderer.render(markdownString)
+  return response
+}
+
 export default function APITest() {
   const { patches } = useLoaderData();
- 
+  const __html = getMarkdownToHTML('/docs/beginner-patches.md')
+
   return (
-    <div>
-      {JSON.stringify(patches)}
-    </div>
+    <div dangerouslySetInnerHTML={{ __html }} />
   );
 }
