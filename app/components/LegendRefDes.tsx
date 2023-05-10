42070a2daddf267c72551a0a907383fab8390ab8
Lars Larsen <31598556+creatorlars@users.noreply.github.com>
2023-05-10 16:55:36 -0700

    Interactive connector legend

--- app/components/LegendRefDes.tsx
+++ app/components/LegendRefDes.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 export interface LegendRefDesProps {
   refDes: string;
+  selected: boolean;
 }
 
 export const LegendRefDes = ({
   refDes = "J1",
+  selected = false,
   ...props
 }: LegendRefDesProps) => {
   return (
-    <div className="bg-pink-500 text-white font-semibold text-sm rounded-full text-center w-7 h-7">
+    <div className={(selected ? "font-bold bg-yellow-500 text-black " : "font-normal bg-gray-700 text-white " ) + " text-sm rounded-full text-center w-7 h-7"}>
       <span className="inline-block py-1">{refDes}</span>
     </div>
   );
