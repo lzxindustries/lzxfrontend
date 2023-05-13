01abe36c48b609798f3559e29a636403bfc9b552
Lars Larsen <31598556+creatorlars@users.noreply.github.com>
2023-05-12 19:24:12 -0700

    WIP

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -10,11 +10,6 @@ const url = "https://lzxindustries.us11.list-manage.com/subscribe/post?u=7da8b11
 export const NewsletterSignup = () =>
   <div className="grid gap-4">
     <Text size="lead">Sign up for our newsletter!</Text><Text size="copy" className="text-gray-400"><MailchimpSubscribe url={url} /></Text>
-    <div className="grid gap-4">
-      <div className="grid gap-4"> <Link target="_blank" to="https://us11.campaign-archive.com/home/?u=7da8b11822c70e5b64240e14f&id=352bd533b6"><IconPolicy className="inline-block align-middle" /> <Text className="inline-block align-middle">LZX Newsletter Archive </Text></Link></div>
-      <div className="grid gap-4"> <Link target="_blank" to="/policies/terms-of-service"><IconPolicy className="inline-block align-middle" /> <Text className="inline-block align-middle">Terms of Service </Text></Link></div>
-      <div className="grid gap-4"> <Link target="_blank" to="/policies/refund-policy"><IconPolicy className="inline-block align-middle" /> <Text className="inline-block align-middle">Refund Policy </Text></Link></div>
-    </div>
-  </div>
+   </div>
 
 export default NewsletterSignup
