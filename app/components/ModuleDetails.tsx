c02ad0933647ffdfd2f0f2beac06b3f622681652
Lars Larsen <31598556+creatorlars@users.noreply.github.com>
2023-05-15 15:11:50 -0700

    WIP

--- app/components/ModuleDetails.tsx
+++ app/components/ModuleDetails.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import { ModuleView } from '~/views/module';
 import { ModuleLegendPanel } from './ModuleLegendPanel';
+import { FaArrowRight } from 'react-icons/fa';
+import { BsFillArrowRightSquareFill } from 'react-icons/bs'
+import { TbRectangleFilled } from 'react-icons/tb'
+import { TbTriangleFilled } from 'react-icons/tb'
+import { TbTriangleInvertedFilled } from 'react-icons/tb';
+import { TbCircleFilled } from 'react-icons/tb'
 
 export function ModuleDetails({
   children,
@@ -29,29 +35,26 @@ export function ModuleDetails({
         <div className="px-8 py-4"><img style={{width: "auto", height: "85vh"}} src={"/images/" + moduleData.frontpanel} /></div>
       </div>
       <div className="basis-[100%] md:basis-1/2 md:h-screen hiddenScroll md:overflow-y-scroll">
-        <article key="ModuleDetailsArticle" className="prose max-w-none px-8">
-          
+        <article key="ModuleDetailsArticle" className="prose prose-sm max-w-none px-8">
           <div className="flex flex-row flex-wrap w-full"><div className="w-[1/2]"><h1>{moduleData.name}</h1>
           {/* <h4 className="top-0">{moduleData.subtitle}</h4> */}
           </div>
           <div className="w-[1/2]">{children}</div></div>
-          
           <p>{moduleData.description}</p>
           {hasMainFeatures ? moduleData.features.map((feature) => {
             return feature.topic == "Main" ? (
               <div>
-                <h3>{feature.name}</h3>
+                <h2><TbRectangleFilled className="inline-block align-middle" /> <span className="align-middle">{feature.name}</span></h2>
                 <p>{feature.description}</p>
               </div>) : ''
           }) : ''}
-          
           <div className="flex flex-wrap flex-row">
             <div className="basis-[100%] md:basis-1/2">
               <h3>Dimensions</h3>
-              <ul>
-                <li>{'Width, ' + moduleData.hp + 'HP'}</li>
-                <li>{'Mounting Depth, ' + moduleData.mounting_depth_mm + 'mm'}</li>
-              </ul>
+              {/* <ul className="list-none"> */}
+                <p>{'Width, ' + moduleData.hp + 'HP'}<br/>
+                {'Mounting Depth, ' + moduleData.mounting_depth_mm + 'mm'}</p>
+              {/* </ul> */}
             </div>
             {(moduleData.has_rear_video_sync_input !== true &&
               moduleData.has_rear_video_sync_output !== true &&
@@ -63,32 +66,32 @@ export function ModuleDetails({
               moduleData.has_rear_14_pin_sync_output !== true) ? '' :
               <div className="basis-[100%] md:basis-1/2">
                 <h3>Video Sync</h3>
-                <ul>
+                <p>
                   {moduleData.has_rear_video_sync_input == true ?
-                    <li>Rear RCA Sync Input Jack</li> : ''
+                    <>Rear RCA Sync Input Jack<br/></> : ''
                   }
                   {moduleData.has_rear_video_sync_output == true ?
-                    <li>Rear RCA Sync Output Jack</li> : ''
+                    <>Rear RCA Sync Output Jack<br/></> : ''
                   }
                   {moduleData.has_front_video_sync_input == true ?
-                    <li>Front RCA Sync Input Jack</li> : ''
+                    <>Front RCA Sync Input Jack<br/></> : ''
                   }
                   {moduleData.has_front_video_sync_output == true ?
-                    <li>Front RCA Sync Output Jack</li> : ''
+                    <>Front RCA Sync Output Jack<br/></> : ''
                   }
                   {moduleData.has_eurorack_power_sync_input == true ?
-                    <li>Rear EuroRack Power Header Sync Input (CV/Gate Bus)</li> : ''
+                    <>Rear EuroRack Power Header Sync Input (CV/Gate Bus)<br/></> : ''
                   }
                   {moduleData.has_eurorack_power_sync_output == true ?
-                    <li>Rear EuroRack Power Header Sync Output (CV/Gate Bus)</li> : ''
+                    <>Rear EuroRack Power Header Sync Output (CV/Gate Bus)<br/></> : ''
                   }
                   {moduleData.has_rear_14_pin_sync_input == true ?
-                    <li>Rear 14 Pin Header Sync Input</li> : ''
+                    <>Rear 14 Pin Header Sync Input<br/></> : ''
                   }
                   {moduleData.has_rear_14_pin_sync_output == true ?
-                    <li>Rear 14 Pin Header Sync Output</li> : ''
+                    <>Rear 14 Pin Header Sync Output<br/></> : ''
                   }
-                </ul>
+                </p>
               </div>}
             {/* {moduleData.is_sync_ref_required == true ?
               <p>Connection to a video sync ref is required to use this module.</p> :
@@ -96,25 +99,25 @@ export function ModuleDetails({
             } */}
             <div className="basis-[100%] md:basis-1/2">
               <h3>Power Consumption</h3>
-              <ul>
+              <p>
                 {moduleData.max_pos_12v_ma !== 0 ?
-                  <li>{'+12V @ ' + moduleData.max_pos_12v_ma + 'mA'}</li> : ''
+                  <>{'+12V @ ' + moduleData.max_pos_12v_ma + 'mA'}<br/></> : ''
                 }
                 {moduleData.max_neg_12v_ma !== 0 ?
-                  <li>{'-12V @ ' + moduleData.max_neg_12v_ma + 'mA'}</li> : ''
+                  <>{'-12V @ ' + moduleData.max_neg_12v_ma + 'mA'}<br/></> : ''
                 }
-              </ul>
+              </p>
             </div>
             <div className="basis-[100%] md:basis-1/2">
               <h3>Power Entry</h3>
-              <ul>
+              <p>
                 {moduleData.has_dc_barrel_power_entry == true ?
-                  <li>Rear DC Barrel Jack</li> : ''
+                  <>Rear DC Barrel Jack<br/></> : ''
                 }
                 {moduleData.has_eurorack_power_entry == true ?
-                  <li>Rear EuroRack 16 Pin Header</li> : ''
+                  <>Rear EuroRack 16 Pin Header<br/></> : ''
                 }
-              </ul>
+              </p>
             </div>
           {(moduleData.connectors.length == 0 && moduleData.controls.length == 0) ? '' : 
             <div className="basis-[100%] xl:basis-1/2 z-0">
@@ -125,10 +128,10 @@ export function ModuleDetails({
             {(moduleData.connectors.length == 0 && moduleData.controls.length == 0) ? '' : 
             <div className="basis-[100%] xl:basis-1/2">
               {moduleData.connectors.length > 0 ? <h3>Connectors</h3> : null}
+              <p>
               {moduleData.connectors.length > 0 ? moduleData.connectors.map((conn) => {
                 return (
-                  
-                    <div
+                    <div 
                       className={"flex flex-row cursor-pointer " +
                         (activeRefDes == conn.refDes ? " bg-yellow-500 text-black bg-opacity-100" : "bg-black text-primary/50 bg-opacity-0")
                       }
@@ -136,7 +139,6 @@ export function ModuleDetails({
                         setActiveRefDes(conn.refDes)
                       }}
                     >
-                      
                       <div className="shrink-0 grow-0 w-10">{conn.refDes}</div><div className="">{conn.name} {conn.is_input ? 'Input' : 'Output'}</div>
                     </div>
                   )
@@ -155,10 +157,11 @@ export function ModuleDetails({
                     </div>
                   )
               }) : null}
+              </p>
             </div>}
           </div>
           {hasPatchFeatures ?
-            <h2>Patching</h2> : ''}
+            <h2><TbRectangleFilled className="inline-block align-middle" /> <span className="align-middle">Patching Tips</span></h2> : ''}
           {hasPatchFeatures ? moduleData.features.map((feature) => {
             return feature.topic == "Patch" ? (
               <div>
@@ -167,7 +170,7 @@ export function ModuleDetails({
               </div>) : ''
           }) : ''}
           {hasSystemFeatures ?
-            <h2>System Building</h2> : ''}
+            <h2><TbRectangleFilled className="inline-block align-middle" /> <span className="align-middle">System Building Tips</span></h2> : ''}
           {hasSystemFeatures ? moduleData.features.map((feature) => {
             return feature.topic == "System" ? (
               <div>
