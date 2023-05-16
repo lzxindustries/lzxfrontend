59a1a073777ae61bbb3851834eaf7ab921faad28
Lars Larsen <31598556+creatorlars@users.noreply.github.com>
2023-05-16 09:16:04 -0700

    Update table view

--- app/routes/($lang).modules.tsx
+++ app/routes/($lang).modules.tsx
@@ -7,10 +7,11 @@ import { Section } from "~/components/Text";
 import { Link } from "@remix-run/react";
 import IconLink from "~/components/IconLink";
 import { routeHeaders, CACHE_SHORT } from '~/data/cache';
+import { BsCheckSquareFill } from "react-icons/bs";
 export const headers = routeHeaders;
 
 export async function loader({ params, request, context }: LoaderArgs) {
-  
+
   const modules = await getAllModules(context)
   return (
     {
@@ -24,38 +25,44 @@ export default function Product() {
   return (
     <>
       <Section className="flex flex-auto justify-center">
-        <table className="max-w-5xl w-auto">
-          <tr>
-            <th className="px-2">Company</th>
-            <th className="px-2">Module</th>
-            <th className="px-2">HP</th>
-            <th className="px-2">Depth</th>
-            <th className="px-2">+12V Current</th>
-            <th className="px-2">-12V Current</th>
-            <th className="px-2">Generates Sync?</th>
-            <th className="px-2">Sync Input Required?</th>
-          </tr>
-          {modules.map((module) => {
-            var showModule = module.max_pos_12v_ma > 0 && module.is_hidden == false && module.hp > 0 ? true : false;
-            return (
-              showModule ? <>
-                <tr>
-                  <td className="px-2">{module.company.name}</td>
-                  {
-                    module.external_url ? <td className="px-2"><Link className="underline" target="_blank" to={module.external_url}>{module.name}<IconLink className="inline-block" /></Link></td> :
-                      <td className="px-2"><Link className="underline" to={'/products/' + module.name.toLowerCase().replace(/\//g, '')}>{module.name}</Link></td>
-                  }
-                  <td className="px-2">{module.hp}HP</td>
-                  <td className="px-2">{module.mounting_depth_mm}mm</td>
-                  <td className="px-2">{module.max_pos_12v_ma}mA</td>
-                  <td className="px-2">{module.max_neg_12v_ma}mA</td>
-                  <td className={module.is_sync_generator ? 'text-green-500 px-2' : 'px-2'}>{module.is_sync_generator ? 'Yes' : 'No'}</td>
-                  <td className={module.is_sync_ref_required ? 'text-green-500 px-2' : 'px-2'}>{module.is_sync_ref_required ? 'Yes' : 'No'}</td>
-                </tr></> : ''
-            )
-          })
-          }
-        </table>
+        <div className="overflow-x-auto">
+          <table className="table table-compact w-full">
+            <thead>
+              <tr>
+                <th >Company</th>
+                <th >Module</th>
+                <th >HP</th>
+                <th >Depth</th>
+                <th >+12V Current</th>
+                <th >-12V Current</th>
+                <th >Generates Sync?</th>
+                <th >Sync Input Required?</th>
+              </tr>
+            </thead>
+            <tbody>
+              {modules.map((module) => {
+                var showModule = module.max_pos_12v_ma > 0 && module.is_hidden == false && module.hp > 0 ? true : false;
+                return (
+                  showModule ? <>
+                    <tr>
+                      <td >{module.company.name}</td>
+                      {
+                        module.external_url ? <td ><Link className="underline" target="_blank" to={module.external_url}>{module.name}<IconLink className="inline-block" /></Link></td> :
+                          <td ><Link className="underline" to={'/products/' + module.name.toLowerCase().replace(/\//g, '')}>{module.name}</Link></td>
+                      }
+                      <td >{module.hp}HP</td>
+                      <td >{module.mounting_depth_mm}mm</td>
+                      <td >{module.max_pos_12v_ma > 0 ? module.max_pos_12v_ma + "mA" : ''}</td>
+                      <td >{module.max_neg_12v_ma > 0 ? module.max_neg_12v_ma + "mA" : ''}</td>
+                      <td className={module.is_sync_generator ? 'text-green-500' : ''}>{module.is_sync_generator ? 'Yes' : ''}</td>
+                      <td className={module.is_sync_ref_required ? 'text-green-500' : ''}>{module.is_sync_ref_required ? 'Yes' : ''}</td>
+                    </tr></> : ''
+                )
+              })
+              }
+            </tbody>
+          </table>
+        </div>
       </Section>
     </>
   );
