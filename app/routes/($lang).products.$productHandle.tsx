43e73ecd7ddfdd17902b0492f3043705ebad621b
daniel <me@dnhaugen.com>
2023-07-21 22:01:45 +0000

    seo notes

--- app/routes/($lang).products.$productHandle.tsx
+++ app/routes/($lang).products.$productHandle.tsx
@@ -92,7 +92,7 @@ export async function loader({ params, request, context }: LoaderArgs) {
   };
 
   const seo = seoPayload.product({
-    product,  // Note: I looked at this value and there is a field in here for seo title and description that is getting pulled from Shopify. Any SEO updates need to be made there. 
+    product,  // Note: there is a field in this value for seo title and description that is getting pulled from Shopify. Any SEO updates need to be made there. 
     selectedVariant,
     url: request.url,
   });
