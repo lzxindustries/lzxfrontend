e20b173012869636cc8966114352c54f1fcd557f
Gary Tyler <mail@garytyler.com>
2024-12-23 14:48:42 -0600

    Add key to list items (patch.name) to fix React warning.

--- app/routes/($lang).patches._index.tsx
+++ app/routes/($lang).patches._index.tsx
@@ -19,53 +19,48 @@ export default function Patches() {
         <Grid layout="patches" className="py-0">
           {patchData.map((patch, it) => {
             return (
-              <>
-                <div>
-                  <Text
-                    size="lead"
-                    className="w-full uppercase"
-                    color="primary"
-                  >
-                    {patch.name}
-                  </Text>
-                  {patch.youtube ? (
-                    <iframe
-                      className="w-full aspect-video"
-                      src={
-                        'https://www.youtube.com/embed/' +
-                        patch.youtube +
-                        '?loop=1'
-                      }
-                      title={patch.name}
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    ></iframe>
-                  ) : (
-                    ''
-                  )}
+              <div key={patch.name}>
+                <Text size="lead" className="w-full uppercase" color="primary">
+                  {patch.name}
+                </Text>
+                {patch.youtube ? (
+                  <iframe
+                    className="w-full aspect-video"
+                    src={
+                      'https://www.youtube.com/embed/' +
+                      patch.youtube +
+                      '?loop=1'
+                    }
+                    title={patch.name}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  ></iframe>
+                ) : (
+                  ''
+                )}
 
-                  {patch.gif && !patch.youtube ? (
-                    <ModalImage
-                      className="opacity-100 w-full align-middle"
-                      smallSrcSet={'/clips/' + patch.gif}
-                      hideDownload={true}
-                      hideZoom={true}
-                      small={'/clips/' + patch.gif}
-                      large={'/clips/' + patch.gif}
-                      alt={patch.name + ' Clip'}
-                    />
-                  ) : (
-                    ''
-                  )}
-                  <div className="w-full h-2" />
-                  {patch.artist.name ? (
-                    <p>
-                      <Text color="primary">Artist </Text>
-                      <Text color="subtle">{patch.artist.name}</Text>
-                    </p>
-                  ) : (
-                    ''
-                  )}
-                  {/* {
+                {patch.gif && !patch.youtube ? (
+                  <ModalImage
+                    className="opacity-100 w-full align-middle"
+                    smallSrcSet={'/clips/' + patch.gif}
+                    hideDownload={true}
+                    hideZoom={true}
+                    small={'/clips/' + patch.gif}
+                    large={'/clips/' + patch.gif}
+                    alt={patch.name + ' Clip'}
+                  />
+                ) : (
+                  ''
+                )}
+                <div className="w-full h-2" />
+                {patch.artist.name ? (
+                  <p>
+                    <Text color="primary">Artist </Text>
+                    <Text color="subtle">{patch.artist.name}</Text>
+                  </p>
+                ) : (
+                  ''
+                )}
+                {/* {
                       patch.artists && (patch.artists.length > 1 && <p><Text color="primary">Artists </Text>
                         <Text color="subtle">
                           {patch.artists.map((artist, index) => {
@@ -84,7 +79,7 @@ export default function Patches() {
                         </Text>
                       </p>
                     } */}
-                  {/* {
+                {/* {
                       patch.modules && (<p><Text color="primary">Modules </Text>
                         <Text color="subtle">
                           {patch.modules.map((module) => {
@@ -93,7 +88,7 @@ export default function Patches() {
                         </Text>
                       </p>)
                     } */}
-                  {/*
+                {/*
                       patch.modules && (patch.modules.length > 0 && <p><Text color="primary">Modules </Text>
                         <Text color="subtle">
                           {patch.modules.map((module, index) => {
@@ -105,25 +100,24 @@ export default function Patches() {
                         </Text>
                       </p>)
                      */}
-                  {patch.diagram && (
-                    <a
-                      href={'/diagrams/' + patch.diagram}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img
-                        src={'/diagrams/' + patch.diagram}
-                        alt={patch.name + ' Patch Diagram'}
-                      />
-                    </a>
-                  )}
-                  {patch.notes && (
-                    <Text size="fine" color="subtle" format>
-                      {patch.notes}
-                    </Text>
-                  )}
-                </div>
-              </>
+                {patch.diagram && (
+                  <a
+                    href={'/diagrams/' + patch.diagram}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={'/diagrams/' + patch.diagram}
+                      alt={patch.name + ' Patch Diagram'}
+                    />
+                  </a>
+                )}
+                {patch.notes && (
+                  <Text size="fine" color="subtle" format>
+                    {patch.notes}
+                  </Text>
+                )}
+              </div>
             );
           })}
         </Grid>
