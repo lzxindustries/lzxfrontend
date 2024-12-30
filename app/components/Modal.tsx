c8b2d93ef34b4a3962c9bcd6daaafb76b5d9079e
Gary Tyler <mail@garytyler.com>
2024-12-30 11:28:12 -0600

    add bg-white class to modal elements

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -10,7 +10,7 @@ export function Modal({
 }) {
   return (
     <div
-      className="relative z-50"
+      className="relative z-50 bg-white"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
@@ -20,7 +20,7 @@ export function Modal({
       <div className="fixed inset-0 z-50 overflow-y-auto">
         <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
           <div
-            className="relative flex-1 px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform rounded shadow-xl bg-contrast sm:my-12 sm:flex-none sm:w-full sm:max-w-sm sm:p-6"
+            className="relative flex-1 px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform rounded shadow-xl bg-white sm:my-12 sm:flex-none sm:w-full sm:max-w-sm sm:p-6"
             role="button"
             onClick={(e) => {
               e.stopPropagation();
