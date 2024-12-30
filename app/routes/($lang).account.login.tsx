77d1fb20f5ed401f14a1c262a02b7ab2c76c7130
Gary Tyler <mail@garytyler.com>
2024-12-29 20:16:16 -0600

    check storefront api's errors array to check if error is user error, since isApiError is deprecated

--- app/routes/($lang).account.login.tsx
+++ app/routes/($lang).account.login.tsx
@@ -66,7 +66,7 @@ export const action: ActionFunction = async ({request, context, params}) => {
       },
     });
   } catch (error: any) {
-    if (storefront.isApiError(error)) {
+    if (error.errors) {
       return badRequest({
         formError: 'Something went wrong. Please try again later.',
       });
