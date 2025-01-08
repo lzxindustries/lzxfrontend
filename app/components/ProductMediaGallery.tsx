15f178922413268b35c768bc4a6e88f38cc259c6
Gary Tyler <mail@garytyler.com>
2025-01-07 20:57:11 -0600

    Fix async cropping

--- app/components/ProductMediaGallery.tsx
+++ app/components/ProductMediaGallery.tsx
@@ -20,10 +20,59 @@ interface MediaGalleryItem {
 
 interface ProductMediaGalleryProps {
   product: Product;
-  moduleData: any;
+  moduleData: ModuleData;
 }
 
-function getLastPathSegment(url: string): string | null {
+interface ModuleData {
+  name: string;
+  videos: {youtube: string; name: string}[];
+}
+
+const getGalleryMedia = (
+  product: Product,
+  moduleData: {
+    name: string;
+    videos: {youtube: string; name: string}[];
+  },
+): MediaGalleryItem[] => {
+  const items: MediaGalleryItem[] = [];
+  const seenYoutubeIds = new Set<string>();
+
+  product.media.nodes.forEach((item: Media, index) => {
+    if (item.mediaContentType === 'IMAGE') {
+      const shopifyImage = item as MediaImage;
+      if (!shopifyImage.image) return;
+      items.push({
+        name: shopifyImage.image.altText || `${moduleData.name} image`,
+        src: shopifyImage.image.url,
+        type: MediaGalleryItemType.IMAGE,
+      } as MediaGalleryItem);
+    } else if (item.mediaContentType === 'EXTERNAL_VIDEO') {
+      const shopifyExternalVideo = item as ExternalVideo;
+      const youtubeId = getLastPathSegment(shopifyExternalVideo.embedUrl);
+      if (!youtubeId) return;
+      seenYoutubeIds.add(youtubeId);
+      items.push({
+        name: `${moduleData.name} video (${shopifyExternalVideo.host})`,
+        src: shopifyExternalVideo.embedUrl,
+        type: MediaGalleryItemType.VIDEO,
+      } as MediaGalleryItem);
+    }
+  });
+
+  moduleData.videos.forEach((video: any) => {
+    if (seenYoutubeIds.has(video.youtube)) return;
+    items.push({
+      name: video.name,
+      src: `https://www.youtube.com/embed/${video.youtube}`,
+      type: MediaGalleryItemType.VIDEO,
+    } as MediaGalleryItem);
+  });
+
+  return items;
+};
+
+const getLastPathSegment = (url: string): string | null => {
   try {
     const parsedUrl = new URL(url);
     const segments = parsedUrl.pathname
@@ -34,11 +83,11 @@ function getLastPathSegment(url: string): string | null {
     console.error('Invalid URL:', error);
     return null;
   }
-}
+};
 
 const croppedCache: Record<string, string> = {};
 
-function cropImageWithCache(src: string): Promise<string> {
+const cropImageWithCache = (src: string): Promise<string> => {
   if (croppedCache[src]) {
     return Promise.resolve(croppedCache[src]);
   }
@@ -46,58 +95,20 @@ function cropImageWithCache(src: string): Promise<string> {
     croppedCache[src] = cropped;
     return cropped;
   });
-}
+};
 
 const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
   product,
   moduleData,
 }) => {
-  const mediaGalleryItems: MediaGalleryItem[] = useMemo(() => {
-    const items: MediaGalleryItem[] = [];
-    const seenYoutubeIds = new Set<string>();
-
-    product.media.nodes.forEach((item: Media, index) => {
-      if (item.mediaContentType === 'IMAGE') {
-        const shopifyImage = item as MediaImage;
-        if (!shopifyImage.image) return;
-        items.push({
-          name: shopifyImage.image.altText || `${moduleData.name} image`,
-          src: shopifyImage.image.url,
-          type: MediaGalleryItemType.IMAGE,
-        } as MediaGalleryItem);
-      } else if (item.mediaContentType === 'EXTERNAL_VIDEO') {
-        const shopifyExternalVideo = item as ExternalVideo;
-        const youtubeId = getLastPathSegment(shopifyExternalVideo.embedUrl);
-        if (!youtubeId) return;
-        seenYoutubeIds.add(youtubeId);
-        items.push({
-          name: `${moduleData.name} video (${shopifyExternalVideo.host})`,
-          src: shopifyExternalVideo.embedUrl,
-          type: MediaGalleryItemType.VIDEO,
-        } as MediaGalleryItem);
-      }
-    });
-
-    moduleData.videos.forEach((video: any) => {
-      if (seenYoutubeIds.has(video.youtube)) return;
-      items.push({
-        name: video.name,
-        src: `https://www.youtube.com/embed/${video.youtube}`,
-        type: MediaGalleryItemType.VIDEO,
-      } as MediaGalleryItem);
-    });
-
-    return items;
-  }, [product.media.nodes, moduleData]);
+  const media: MediaGalleryItem[] = useMemo(() => {
+    return getGalleryMedia(product, moduleData);
+  }, [product, moduleData]);
 
   const [currentSlide, setCurrentSlide] = useState(0);
-  const nextSlide = () =>
-    setCurrentSlide((prev) => (prev + 1) % mediaGalleryItems.length);
+  const nextSlide = () => setCurrentSlide((prev) => (prev + 1) % media.length);
   const prevSlide = () =>
-    setCurrentSlide(
-      (prev) =>
-        (prev - 1 + mediaGalleryItems.length) % mediaGalleryItems.length,
-    );
+    setCurrentSlide((prev) => (prev - 1 + media.length) % media.length);
 
   const [preCroppedImages, setPreCroppedImages] = useState<(string | null)[]>(
     [],
@@ -105,44 +116,27 @@ const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
 
   useEffect(() => {
     let isMounted = true;
-    setPreCroppedImages(Array(mediaGalleryItems.length).fill(null));
+    setPreCroppedImages(Array(media.length).fill(null));
 
     (async function processImages() {
-      // Crop item first
-      const currentItem = mediaGalleryItems[currentSlide];
-      if (currentItem && currentItem.type === MediaGalleryItemType.IMAGE) {
-        const croppedSrc = await cropImageWithCache(currentItem.src);
-        if (isMounted) {
-          setPreCroppedImages((prev) => {
-            const next = [...prev];
-            next[currentSlide] = croppedSrc;
-            return next;
-          });
-        }
-      }
-      // Crop other items
-      const otherIndices = mediaGalleryItems
-        .map((_, idx) => idx)
-        .filter((idx) => idx !== currentSlide);
-      for (const idx of otherIndices) {
-        const item = mediaGalleryItems[idx];
-        if (item.type === MediaGalleryItemType.IMAGE) {
-          const croppedSrc = await cropImageWithCache(item.src);
-          if (isMounted) {
-            setPreCroppedImages((prev) => {
-              const next = [...prev];
-              next[idx] = croppedSrc;
-              return next;
-            });
+      const croppedImages = await Promise.all(
+        media.map(async (item) => {
+          if (item.type === MediaGalleryItemType.IMAGE) {
+            return await cropImageWithCache(item.src);
           }
-        }
+          return null;
+        }),
+      );
+
+      if (isMounted) {
+        setPreCroppedImages(croppedImages);
       }
     })();
 
     return () => {
       isMounted = false;
     };
-  }, [mediaGalleryItems, currentSlide]);
+  }, [media, currentSlide]);
 
   return (
     <div className="w-full lg:w-1/2 card-image">
@@ -153,7 +147,7 @@ const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
             className="mb-0 p-0 text-black rounded-full bg-white any-hover:hover:bg-black any-hover:hover:text-white border border-gray-500 transition-colors duration-200 md:p-1 lg:p-1 m-0 lg:m-1"
             aria-label="Previous Slide"
             style={{
-              visibility: mediaGalleryItems.length <= 1 ? 'hidden' : 'visible',
+              visibility: media.length <= 1 ? 'hidden' : 'visible',
             }}
           >
             <svg
@@ -172,23 +166,21 @@ const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
             </svg>
           </button>
           <div className="flex justify-center w-full h-full p-1 lg:p-2 overflow-hidden ">
-            {mediaGalleryItems[currentSlide].type ===
-            MediaGalleryItemType.IMAGE ? (
+            {media[currentSlide].type === MediaGalleryItemType.IMAGE ? (
               preCroppedImages[currentSlide] ? (
                 <img
                   className="w-full h-full object-contain"
                   src={preCroppedImages[currentSlide]}
-                  alt={mediaGalleryItems[currentSlide].name}
+                  alt={media[currentSlide].name}
                 />
               ) : null
-            ) : mediaGalleryItems[currentSlide].type ===
-              MediaGalleryItemType.VIDEO ? (
+            ) : media[currentSlide].type === MediaGalleryItemType.VIDEO ? (
               <div className="w-full ">
                 <div className="relative inset-y-[25%]">
                   <iframe
                     className="aspect-video w-full "
-                    src={mediaGalleryItems[currentSlide].src}
-                    title={mediaGalleryItems[currentSlide].name}
+                    src={media[currentSlide].src}
+                    title={media[currentSlide].name}
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
                   />
                 </div>
@@ -200,7 +192,7 @@ const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
             className="mb-0 p-0 text-black rounded-full bg-white any-hover:hover:bg-black any-hover:hover:text-white border border-gray-500 transition-colors duration-200 md:p-1 lg:p-1 m-0 lg:m-1"
             aria-label="Next Slide"
             style={{
-              visibility: mediaGalleryItems.length <= 1 ? 'hidden' : 'visible',
+              visibility: media.length <= 1 ? 'hidden' : 'visible',
             }}
           >
             <svg
@@ -219,21 +211,17 @@ const ProductMediaGallery: React.FC<ProductMediaGalleryProps> = ({
             </svg>
           </button>
         </div>
-        {mediaGalleryItems.length > 1 && (
+        {media.length > 1 && (
           <div
             className="flex justify-center items-center mb-2 mt-0 pt-0"
             style={{
-              visibility: mediaGalleryItems.length <= 1 ? 'hidden' : 'visible',
+              visibility: media.length <= 1 ? 'hidden' : 'visible',
             }}
           >
             <div className="inline-flex justify-center items-center bg-white rounded-full any-hover:hover:bg-gray-100 border border-gray-500 transition-colors duration-200 p-2 mx-auto">
-              {mediaGalleryItems.map((_, index) => (
+              {media.map((_, index) => (
                 <button
-                  key={
-                    mediaGalleryItems.length > 1
-                      ? mediaGalleryItems[index].name
-                      : ''
-                  }
+                  key={media.length > 1 ? media[index].name : ''}
                   onClick={() => setCurrentSlide(index)}
                   className={`w-3 h-3 mx-1 rounded-full any-hover:hover:bg-black ${
                     index === currentSlide ? 'bg-black' : 'bg-gray-300'
