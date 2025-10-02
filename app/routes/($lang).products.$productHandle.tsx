b8112e5d89cccaf24a4296ba60a81f1409f84e91
Lars Larsen <lars@lzxindustries.net>
2025-10-02 11:21:17 -0700

    Revert "Another attempt."
    This reverts commit 7cbee256703b5e9c1ef33a4b3916fec3928b9f81.

--- app/routes/($lang).products.$productHandle.tsx
+++ app/routes/($lang).products.$productHandle.tsx
@@ -6,10 +6,11 @@ import {
   useSearchParams,
 } from '@remix-run/react';
 import type {
+  SeoConfig,
   ShopifyAnalyticsProduct,
   Storefront,
 } from '@shopify/hydrogen';
-import {AnalyticsPageType, Money} from '@shopify/hydrogen';
+import {AnalyticsPageType, getSeoMeta, Money} from '@shopify/hydrogen';
 import type {
   ProductConnection,
   Product as ProductType,
@@ -17,7 +18,7 @@ import type {
   SelectedOptionInput,
   Shop,
 } from '@shopify/hydrogen/storefront-api-types';
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import type {LoaderFunctionArgs, MetaArgs} from '@shopify/remix-oxygen';
 import {defer} from '@shopify/remix-oxygen';
 import clsx from 'clsx';
 import {type ReactNode, useMemo, useRef} from 'react';
@@ -134,6 +135,11 @@ export default function Product() {
         </div>
       </>
     )
+  );
+}
+    <ModuleDetails moduleData={moduleData} product={product}>
+      <ProductForm />
+    </ModuleDetails>
   );
 }
 
